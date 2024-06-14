842e4fe9deb4699039dd8f87e0070e3867c59b75
Johnny Luo <johnnyluo1980@gmail.com>
2024-06-14 12:28:01 +1000

    Update swift tool version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "walletcore",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v14_5),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
