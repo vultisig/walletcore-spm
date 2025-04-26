6c988a4d2f8b5212e9751d28391600e8a5720c04
Johnny Luo <johnnyluo1980@gmail.com>
2025-04-26 19:58:44 +1000

    update package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "walletcore",
+    name: "WalletCore",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "walletcore",
+            name: "WalletCoreCommon",
             targets: ["WalletCoreCommon"]),
     ],
     dependencies: [
