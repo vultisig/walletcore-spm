7cdf4dc6c54fa1462cda8f31330298fa3dcb07b4
Johnny Luo <johnnyluo1980@gmail.com>
2025-09-07 22:05:02 +1000

    Update Swift tools version and modify package dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
+        .package(url: "https://github.com/vultisig/WalletCoreSwiftProtobuf.git" , branch: "main"),
     ],
     targets: [
         .binaryTarget(
